383d264f7e46036f4820b568476fb24792466825
Shander Lam <shanderlam@gmail.com>
2018-01-11 15:56:45 +0800

    Enabled config is not necessary
    You can disable this hook by setting `sails.hooks.morgan = false`

--- index.js
+++ index.js
@@ -1,27 +1,15 @@
 const morgan = require('morgan');
 
 module.exports = function (sails) {
-  let logger = function(req, res, next) {
-    return next();
-  };
   return {
-    initialize: function(cb) {
-      if (sails.config[this.configKey].enabled) {
-        logger = morgan(sails.config[this.configKey].format);
-      }
-      return cb();
-    },
     defaults: {
       __configKey__: {
-        enabled: false,
         format: 'dev'
       }
     },
     routes: {
       before: {
-        '*': function(req, res, next) {
-          logger(req, res, next);
-        }
+        '*': morgan(sails.config[this.configKey].format)
       }
     }
   };
